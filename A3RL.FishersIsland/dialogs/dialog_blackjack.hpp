3148308c7e9e55d21bdfb5e5de8f396759ca98f5
Muse <kaydendeason@gmail.com>
2020-05-05 15:27:52 -0400

    Blackjack ui basics

---
+++ A3RL.FishersIsland/dialogs/dialog_blackjack.hpp
@@ -0,0 +1,61 @@
+/* #Vajoku Non-split
+$[
+	1.063,
+	["blackjack",[[0,0,1,1],0.025,0.04,"GUI_GRID"],0,0,0],
+	[-1200,"",[1,"\A3PL_Common\gui\blackjack\background.paa",["-0.129062 * safezoneW + safezoneX","-0.028 * safezoneH + safezoneY","1.25297 * safezoneW","1.166 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1201,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.200937 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1202,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.309219 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1203,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.422656 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1204,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.536094 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1205,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.649531 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1207,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.314375 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1208,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.427812 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1209,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.54125 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1210,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.654688 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1206,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.206094 * safezoneW + safezoneX","0.522 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1000,"",[1,"",["0.741823 * safezoneW + safezoneX","0.436778 * safezoneH + safezoneY","0.0521355 * safezoneW","0.0147037 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1001,"",[1,"",["0.751614 * safezoneW + safezoneX","0.475222 * safezoneH + safezoneY","0.0417708 * safezoneW","0.0161111 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1211,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.37625 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1212,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.438125 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1213,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.5 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1214,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.561875 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1600,"",[1,"",["0.37625 * safezoneW + safezoneX","0.763815 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1601,"",[1,"",["0.4375 * safezoneW + safezoneX","0.763889 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1602,"",[1,"",["0.5 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1603,"",[1,"",["0.561875 * safezoneW + safezoneX","0.764 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]]
+]
+*/
+
+/* #Lymigy Split
+$[
+	1.063,
+	["blackjack",[[0,0,1,1],0.025,0.04,"GUI_GRID"],0,0,0],
+	[-1200,"",[1,"\A3PL_Common\gui\blackjack\background.paa",["-0.129062 * safezoneW + safezoneX","-0.028 * safezoneH + safezoneY","1.25297 * safezoneW","1.166 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1201,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.200937 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1202,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.309219 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1203,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.422656 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1204,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.536094 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1205,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.649531 * safezoneW + safezoneX","0.203 * safezoneH + safezoneY","0.0928125 * safezoneW","0.198 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1000,"",[1,"",["0.741823 * safezoneW + safezoneX","0.436778 * safezoneH + safezoneY","0.0521355 * safezoneW","0.0147037 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1001,"",[1,"",["0.751614 * safezoneW + safezoneX","0.475222 * safezoneH + safezoneY","0.0417708 * safezoneW","0.0161111 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1206,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.200937 * safezoneW + safezoneX","0.467 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1207,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.262812 * safezoneW + safezoneX","0.467 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1208,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.324687 * safezoneW + safezoneX","0.467 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1209,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.386562 * safezoneW + safezoneX","0.467 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1210,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.448438 * safezoneW + safezoneX","0.467 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1600,"",[1,"",["0.520625 * safezoneW + safezoneX","0.489 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1211,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.520625 * safezoneW + safezoneX","0.489 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1212,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.587656 * safezoneW + safezoneX","0.489 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1601,"",[1,"",["0.587656 * safezoneW + safezoneX","0.489 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1213,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.200937 * safezoneW + safezoneX","0.621 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1214,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.262812 * safezoneW + safezoneX","0.621 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1215,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.324687 * safezoneW + safezoneX","0.621 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1216,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.386562 * safezoneW + safezoneX","0.621 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1217,"",[1,"\A3PL_Common\gui\blackjack\cards\5C.paa",["0.448438 * safezoneW + safezoneX","0.621 * safezoneH + safezoneY","0.0515625 * safezoneW","0.11 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1218,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.520625 * safezoneW + safezoneX","0.643 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1219,"",[1,"\A3PL_Common\gui\blackjack\hit.paa",["0.587656 * safezoneW + safezoneX","0.643 * safezoneH + safezoneY","0.0464063 * safezoneW","0.077 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1602,"",[1,"",["0.587656 * safezoneW + safezoneX","0.643 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]],
+	[1603,"",[1,"",["0.520625 * safezoneW + safezoneX","0.643 * safezoneH + safezoneY","0.0469792 * safezoneW","0.0781482 * safezoneH"],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],"","-1"],[]]
+]
+*/
+
